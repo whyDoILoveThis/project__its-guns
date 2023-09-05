1f4f4831d932877da4bb370e93eff974a45dcbe9
Ian Smith <iannsmif@gmail.com>
2023-09-05 13:29:59 -0500

    BUILD: main banner

---
+++ components/banner-main/banner-main.js
@@ -0,0 +1,15 @@
+const banner = document.querySelector('.banner');
+const bannerSection = banner.querySelectorAll('.banner__section');
+let currentIndex = 0;
+
+function showBannerSection(index) {
+    bannerSection[currentIndex].classList.remove('active');
+    currentIndex = (index + bannerSection.length) % bannerSection.length;
+    bannerSection[currentIndex].classList.add('active');
+}
+
+function nextSection() {
+    showBannerSection(currentIndex + 1);
+}
+
+setInterval(nextSection, 5000); // Change image every 3 seconds (adjust as needed)
